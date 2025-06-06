bbfd7467675c9d311fb94060c7ffa987c4214dc6
Blazej Sleboda <5544365+Adobels@users.noreply.github.com>
2025-06-06 17:26:23 +0200

    Rename targets

--- Package.swift
+++ Package.swift
@@ -6,14 +6,14 @@ import PackageDescription
 let package = Package(
     name: "StoryboardDecoder",
     products: [
-        .library(name: "StoryboardDecoder", targets: ["Decoder"])
+        .library(name: "StoryboardDecoder", targets: ["StoryboardDecoder"])
     ],
     dependencies: [
         .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "7.0.2")
     ],
     targets: [
-        .target(name: "Decoder", dependencies: ["SWXMLHash"], path: "Sources"),
-        .testTarget(name: "DecoderTests", dependencies: ["Decoder"]),
-        .testTarget(name: "DiscoverTests", dependencies: ["Decoder"])
+        .target(name: "StoryboardDecoder", dependencies: ["SWXMLHash"], path: "Sources"),
+        .testTarget(name: "DecoderTests", dependencies: ["StoryboardDecoder"]),
+        .testTarget(name: "DiscoverTests", dependencies: ["StoryboardDecoder"])
     ]
 )
