ca3dbb432b2105d0c97ca0ae76871a2b7f5c253b
Blazej Sleboda <5544365+Adobels@users.noreply.github.com>
2025-07-03 12:00:37 +0200

    Add support for View properties decoded by its decoder

--- Sources/Models/Views/Controls/PageControl.swift
+++ Sources/Models/Views/Controls/PageControl.swift
@@ -17,146 +17,130 @@ protocol PageControlProtocol: ViewProtocol, ControlProtocol {
 }
 
 public struct PageControl: IBDecodable, PageControlProtocol, IBIdentifiable {
+    // MARK: UIView
+    public let key: String?
     public let id: String
     public let elementClass: String = "UIPageControl"
-
-    public let key: String?
-    public let autoresizingMask: AutoresizingMask?
-    public let clipsSubviews: Bool?
-    public let constraints: [Constraint]?
-    public let contentMode: String?
     public let customClass: String?
     public let customModule: String?
     public let customModuleProvider: String?
     public let restorationIdentifier: String?
+    public let userDefinedRuntimeAttributes: [UserDefinedRuntimeAttribute]?
     public let userLabel: String?
     public let colorLabel: String?
-    public let isMisplaced: Bool?
-    public let isAmbiguous: Bool?
-    public let verifyAmbiguity: VerifyAmbiguity?
-    public let opaque: Bool?
-    public let rect: Rect?
-    public let subviews: [AnyView]?
-    public let translatesAutoresizingMaskIntoConstraints: Bool?
+    public let accessibility: Accessibility?
+    public let contentMode: String?
+    public let semanticContentAttribute: String?
+    public let tag: Int?
     public let userInteractionEnabled: Bool?
-    public let viewLayoutGuide: LayoutGuide?
-    public let userDefinedRuntimeAttributes: [UserDefinedRuntimeAttribute]?
-    public let connections: [AnyConnection]?
-    public let variations: [Variation]?
+    public let multipleTouchEnabled: Bool?
+    public let alpha: Float?
     public let backgroundColor: Color?
     public let tintColor: Color?
+    public let opaque: Bool?
     public let hidden: Bool?
-    public let alpha: Float?
-
-    public let isEnabled: Bool?
-    public let isHighlighted: Bool?
-    public let isSelected: Bool?
-    public let contentHorizontalAlignment: String?
-    public let contentVerticalAlignment: String?
-    public let horizontalCompressionResistancePriority: Int?
-    public let verticalCompressionResistancePriority: Int?
-    public let horizontalHuggingPriority: Int?
-    public let verticalHuggingPriority: Int?
-
-    public let accessibility: Accessibility?
-    public let numberOfPages: Int?
-    public let pageIndicatorTintColor: Color?
-    public let currentPageIndicatorTintColor: Color?
-    public let tag: Int?
-    public let autoresizesSubviews: Bool?
     public let clearsContextBeforeDrawing: Bool?
-    public let multipleTouchEnabled: Bool?
-    public let semanticContentAttribute: String?
+    public let clipsSubviews: Bool?
+    public let autoresizesSubviews: Bool?
+    public let rect: Rect?
+    public let translatesAutoresizingMaskIntoConstraints: Bool?
+    public let autoresizingMask: AutoresizingMask?
+    public let directionalLayoutMargins: DirectionalEdgeInsets?
+    public let layoutMargins: EdgeInset?
     public let preservesSuperviewLayoutMargins: Bool?
     public let layoutMarginsFollowReadableWidth: Bool?
     public let insetsLayoutMarginsFromSafeArea: Bool?
-    public let directionalLayoutMargins: DirectionalEdgeInsets?
-    public let layoutMargins: EdgeInset?
-    public let toolTip: String?
+    public let horizontalHuggingPriority: Int?
+    public let verticalHuggingPriority: Int?
+    public let horizontalCompressionResistancePriority: Int?
+    public let verticalCompressionResistancePriority: Int?
+    public let constraints: [Constraint]?
+    public let connections: [AnyConnection]?
+    public let variations: [Variation]?
+    public let subviews: [AnyView]?
+    public let verifyAmbiguity: VerifyAmbiguity?
+    public let isMisplaced: Bool?
+    public let isAmbiguous: Bool?
+    // MARK: UIControl
+    public let contentHorizontalAlignment: String?
+    public let contentVerticalAlignment: String?
     public let showsMenuAsPrimaryAction: Bool?
+    public let isSelected: Bool?
+    public let isEnabled: Bool?
+    public let isHighlighted: Bool?
+    public let toolTip: String?
+    // MARK: UIPageControl
+    public let numberOfPages: Int?
+    public let pageIndicatorTintColor: Color?
+    public let currentPageIndicatorTintColor: Color?
     public let currentPage: Int?
     public let hidesForSinglePage: Bool?
     public let defersCurrentPageDisplay: Bool?
 
-    enum ConstraintsCodingKeys: CodingKey { case constraint }
-    enum VariationCodingKey: CodingKey { case variation }
-    enum ExternalCodingKeys: CodingKey { case color }
-    enum ColorsCodingKeys: CodingKey { case key }
+    enum PageControlCodingKeys: CodingKey { case color }
+    enum KeyCodingKeys: CodingKey { case key }
 
     static func decode(_ xml: XMLIndexerType) throws -> PageControl {
-        let container = xml.container(keys: MappedCodingKey.self).map { (key: CodingKeys) in
-            let stringValue: String = {
-                switch key {
-                case .isEnabled: return "enabled"
-                case .isHighlighted: return "highlighted"
-                case .isSelected: return "selected"
-                default: return key.stringValue
-                }
-            }()
-            return MappedCodingKey(stringValue: stringValue)
-        }
-        let constraintsContainer = container.nestedContainerIfPresent(of: .constraints, keys: ConstraintsCodingKeys.self)
-        let variationContainer = xml.container(keys: VariationCodingKey.self)
-        let colorsContainer = xml.container(keys: ExternalCodingKeys.self)
-            .nestedContainerIfPresent(of: .color, keys: ColorsCodingKeys.self)
-
+        let view = try View.decode(xml)
+        let control = try Control.decode(xml)
+        let container = xml.container(keys: CodingKeys.self)
+        let colorsContainer = xml.container(keys: PageControlCodingKeys.self).nestedContainerIfPresent(of: .color, keys: KeyCodingKeys.self)
         return PageControl(
-            id:                                        try container.attribute(of: .id),
-            key:                                       container.attributeIfPresent(of: .key),
-            autoresizingMask:                          container.elementIfPresent(of: .autoresizingMask),
-            clipsSubviews:                             container.attributeIfPresent(of: .clipsSubviews),
-            constraints:                               constraintsContainer?.elementsIfPresent(of: .constraint),
-            contentMode:                               container.attributeIfPresent(of: .contentMode),
-            customClass:                               container.attributeIfPresent(of: .customClass),
-            customModule:                              container.attributeIfPresent(of: .customModule),
-            customModuleProvider:                      container.attributeIfPresent(of: .customModuleProvider),
-            restorationIdentifier:                     container.attributeIfPresent(of: .restorationIdentifier),
-            userLabel:                                 container.attributeIfPresent(of: .userLabel),
-            colorLabel:                                container.attributeIfPresent(of: .colorLabel),
-            isMisplaced:                               container.attributeIfPresent(of: .isMisplaced),
-            isAmbiguous:                               container.attributeIfPresent(of: .isAmbiguous),
-            verifyAmbiguity:                           container.attributeIfPresent(of: .verifyAmbiguity),
-            opaque:                                    container.attributeIfPresent(of: .opaque),
-            rect:                                      container.elementIfPresent(of: .rect),
-            subviews:                                  container.childrenIfPresent(of: .subviews),
-            translatesAutoresizingMaskIntoConstraints: container.attributeIfPresent(of: .translatesAutoresizingMaskIntoConstraints),
-            userInteractionEnabled:                    container.attributeIfPresent(of: .userInteractionEnabled),
-            viewLayoutGuide:                           container.elementIfPresent(of: .viewLayoutGuide),
-            userDefinedRuntimeAttributes:              container.childrenIfPresent(of: .userDefinedRuntimeAttributes),
-            connections:                               container.childrenIfPresent(of: .connections),
-            variations:                                variationContainer.elementsIfPresent(of: .variation),
-            backgroundColor:                           colorsContainer?.withAttributeElement(.key, CodingKeys.backgroundColor.stringValue),
-            tintColor:                                 colorsContainer?.withAttributeElement(.key, CodingKeys.tintColor.stringValue),
-            hidden:                                  container.attributeIfPresent(of: .hidden),
-            alpha:                                     container.attributeIfPresent(of: .alpha),
-            isEnabled:                                 container.attributeIfPresent(of: .isEnabled),
-            isHighlighted:                             container.attributeIfPresent(of: .isHighlighted),
-            isSelected:                                container.attributeIfPresent(of: .isSelected),
-            contentHorizontalAlignment:                container.attributeIfPresent(of: .contentHorizontalAlignment),
-            contentVerticalAlignment:                  container.attributeIfPresent(of: .contentVerticalAlignment),
-            horizontalCompressionResistancePriority:   container.attributeIfPresent(of: .horizontalCompressionResistancePriority),
-            verticalCompressionResistancePriority:     container.attributeIfPresent(of: .verticalCompressionResistancePriority),
-            horizontalHuggingPriority:                 container.attributeIfPresent(of: .horizontalHuggingPriority),
-            verticalHuggingPriority:                   container.attributeIfPresent(of: .verticalHuggingPriority),
-            accessibility:                             container.elementIfPresent(of: .accessibility),
-            numberOfPages:                             container.attributeIfPresent(of: .numberOfPages),
-            pageIndicatorTintColor:                    colorsContainer?.withAttributeElement(.key, CodingKeys.pageIndicatorTintColor.stringValue),
-            currentPageIndicatorTintColor:             colorsContainer?.withAttributeElement(.key, CodingKeys.currentPageIndicatorTintColor.stringValue),
-            tag:                                       container.attributeIfPresent(of: .tag),
-            autoresizesSubviews:                       container.attributeIfPresent(of: .autoresizesSubviews),
-            clearsContextBeforeDrawing:                container.attributeIfPresent(of: .clearsContextBeforeDrawing),
-            multipleTouchEnabled:                      container.attributeIfPresent(of: .multipleTouchEnabled),
-            semanticContentAttribute:                  container.attributeIfPresent(of: .semanticContentAttribute),
-            preservesSuperviewLayoutMargins:           container.attributeIfPresent(of: .preservesSuperviewLayoutMargins),
-            layoutMarginsFollowReadableWidth:          container.attributeIfPresent(of: .layoutMarginsFollowReadableWidth),
-            insetsLayoutMarginsFromSafeArea:           container.attributeIfPresent(of: .insetsLayoutMarginsFromSafeArea),
-            directionalLayoutMargins:                     container.elementIfPresent(of: .insetsLayoutMarginsFromSafeArea),
-            layoutMargins:                                 container.elementIfPresent(of: .layoutMargins),
-            toolTip:                                   container.attributeIfPresent(of: .toolTip),
-            showsMenuAsPrimaryAction:                  container.attributeIfPresent(of: .showsMenuAsPrimaryAction),
-            currentPage:                               container.attributeIfPresent(of: .currentPage),
-            hidesForSinglePage:                        container.attributeIfPresent(of: .hidesForSinglePage),
-            defersCurrentPageDisplay: container.attributeIfPresent(of: .defersCurrentPageDisplay),
+            key: view.key,
+            id: view.id,
+            customClass: view.customClass,
+            customModule: view.customModule,
+            customModuleProvider: view.customModuleProvider,
+            restorationIdentifier: view.restorationIdentifier,
+            userDefinedRuntimeAttributes: view.userDefinedRuntimeAttributes,
+            userLabel: view.userLabel,
+            colorLabel: view.colorLabel,
+            accessibility: view.accessibility,
+            contentMode: view.contentMode,
+            semanticContentAttribute: view.semanticContentAttribute,
+            tag: view.tag,
+            userInteractionEnabled: view.userInteractionEnabled,
+            multipleTouchEnabled: view.multipleTouchEnabled,
+            alpha: view.alpha,
+            backgroundColor: view.backgroundColor,
+            tintColor: view.tintColor,
+            opaque: view.opaque,
+            hidden: view.hidden,
+            clearsContextBeforeDrawing: view.clearsContextBeforeDrawing,
+            clipsSubviews: view.clipsSubviews,
+            autoresizesSubviews: view.autoresizesSubviews,
+            rect: view.rect,
+            translatesAutoresizingMaskIntoConstraints: view.translatesAutoresizingMaskIntoConstraints,
+            autoresizingMask: view.autoresizingMask,
+            directionalLayoutMargins: view.directionalLayoutMargins,
+            layoutMargins: view.layoutMargins,
+            preservesSuperviewLayoutMargins: view.preservesSuperviewLayoutMargins,
+            layoutMarginsFollowReadableWidth: view.layoutMarginsFollowReadableWidth,
+            insetsLayoutMarginsFromSafeArea: view.insetsLayoutMarginsFromSafeArea,
+            horizontalHuggingPriority: view.horizontalHuggingPriority,
+            verticalHuggingPriority: view.verticalHuggingPriority,
+            horizontalCompressionResistancePriority: view.horizontalCompressionResistancePriority,
+            verticalCompressionResistancePriority: view.verticalCompressionResistancePriority,
+            constraints: view.constraints,
+            connections: view.connections,
+            variations: view.variations,
+            subviews: view.subviews,
+            verifyAmbiguity: view.verifyAmbiguity,
+            isMisplaced: view.isMisplaced,
+            isAmbiguous: view.isAmbiguous,
+            contentHorizontalAlignment: control.contentHorizontalAlignment,
+            contentVerticalAlignment: control.contentVerticalAlignment,
+            showsMenuAsPrimaryAction: control.showsMenuAsPrimaryAction,
+            isSelected: control.isSelected,
+            isEnabled: control.isEnabled,
+            isHighlighted: control.isHighlighted,
+            toolTip: control.toolTip,
+            numberOfPages: container.attributeIfPresent(of: .numberOfPages),
+            pageIndicatorTintColor: colorsContainer?.withAttributeElement(.key, CodingKeys.pageIndicatorTintColor.stringValue),
+            currentPageIndicatorTintColor: colorsContainer?.withAttributeElement(.key, CodingKeys.currentPageIndicatorTintColor.stringValue),
+            currentPage: container.attributeIfPresent(of: .currentPage),
+            hidesForSinglePage: container.attributeIfPresent(of: .hidesForSinglePage),
+            defersCurrentPageDisplay: container.attributeIfPresent(of: .defersCurrentPageDisplay)
         )
     }
 }
