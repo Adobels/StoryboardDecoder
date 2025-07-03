253991bbc86e028b56ce0699eebea730ad1f2f39
Blazej Sleboda <5544365+Adobels@users.noreply.github.com>
2025-07-03 10:55:22 +0200

    clean up StackView

--- Sources/Models/Views/StackView.swift
+++ Sources/Models/Views/StackView.swift
@@ -17,54 +17,62 @@ protocol StackViewProtocol {
 
 public struct StackView: IBDecodable, ViewProtocol, StackViewProtocol, IBIdentifiable {
 
+    public let key: String?
+    // MARK: Identity Inspector
     public let id: String
     public let elementClass: String = "UIStackView"
-    public let key: String?
-    public let autoresizingMask: AutoresizingMask?
-    public let clipsSubviews: Bool?
-    public let constraints: [Constraint]?
-    public let contentMode: String?
     public let customClass: String?
     public let customModule: String?
     public let customModuleProvider: String?
     public let restorationIdentifier: String?
+    public let userDefinedRuntimeAttributes: [UserDefinedRuntimeAttribute]?
     public let userLabel: String?
     public let colorLabel: String?
-    public let isMisplaced: Bool?
-    public let isAmbiguous: Bool?
-    public let verifyAmbiguity: VerifyAmbiguity?
-    public let opaque: Bool?
-    public let rect: Rect?
-    public let subviews: [AnyView]?
-    public let translatesAutoresizingMaskIntoConstraints: Bool?
+    public let accessibility: Accessibility?
+    // MARK: Attribut Inspector
+    public let contentMode: String?
+    public let semanticContentAttribute: String?
+    public let tag: Int?
     public let userInteractionEnabled: Bool?
-    public let userDefinedRuntimeAttributes: [UserDefinedRuntimeAttribute]?
-    public let connections: [AnyConnection]?
-    public let variations: [Variation]?
+    public let multipleTouchEnabled: Bool?
+    public let alpha: Float?
     public let backgroundColor: Color?
     public let tintColor: Color?
+    public let opaque: Bool?
     public let hidden: Bool?
-    public let alpha: Float?
+    public let clearsContextBeforeDrawing: Bool?
+    public let clipsSubviews: Bool?
+    public let autoresizesSubviews: Bool?
+    // MARK: Size Inspector
+    public let rect: Rect?
+    public let translatesAutoresizingMaskIntoConstraints: Bool?
+    public let autoresizingMask: AutoresizingMask?
+    public let directionalLayoutMargins: DirectionalEdgeInsets?
+    public let layoutMargins: EdgeInset?
+    public let preservesSuperviewLayoutMargins: Bool?
+    public let layoutMarginsFollowReadableWidth: Bool?
+    public let insetsLayoutMarginsFromSafeArea: Bool?
+    /*
+    public let viewLayoutGuide: LayoutGuide? // property specific to UIView in IB
+    public let keyboardLayoutGuide: LayoutGuide? // propserty specific to UIView in IB
+     */
+    public let horizontalHuggingPriority: Int?
+    public let verticalHuggingPriority: Int?
+    public let horizontalCompressionResistancePriority: Int?
+    public let verticalCompressionResistancePriority: Int?
+    public let constraints: [Constraint]?
+    public let connections: [AnyConnection]?
+    public let variations: [Variation]?
+    public let subviews: [AnyView]?
+    public let verifyAmbiguity: VerifyAmbiguity?
+    public let isMisplaced: Bool?
+    public let isAmbiguous: Bool?
+    // MARK: StackView Attributes Inspector
     public let axis: String
     public let distribution: String?
     public let alignment: String?
     public let spacing: Int?
     public let baselineRelativeArrangement: Bool?
-    public let horizontalCompressionResistancePriority: Int?
-    public let verticalCompressionResistancePriority: Int?
-    public let horizontalHuggingPriority: Int?
-    public let verticalHuggingPriority: Int?
-    public let accessibility: Accessibility?
-    public let tag: Int?
-    public let autoresizesSubviews: Bool?
-    public let clearsContextBeforeDrawing: Bool?
-    public let multipleTouchEnabled: Bool?
-    public let semanticContentAttribute: String?
-    public let preservesSuperviewLayoutMargins: Bool?
-    public let layoutMarginsFollowReadableWidth: Bool?
-    public let insetsLayoutMarginsFromSafeArea: Bool?
-    public let directionalLayoutMargins: DirectionalEdgeInsets?
-    public let layoutMargins: EdgeInset?
     /* TODO: Add support for viewLayoutGuide
      <viewLayoutGuide key="safeArea" id="hJ6-lb-hKq"/>
      <keyboardLayoutGuide key="keyboard" id="XH4-lu-ej3"/>
@@ -74,53 +82,53 @@ public struct StackView: IBDecodable, ViewProtocol, StackViewProtocol, IBIdentif
         let view = try View.decode(xml)
         let container = xml.container(keys: CodingKeys.self)
         return StackView(
-            id:                                        view.id,
             key:                                       view.key,
-            autoresizingMask:                          view.autoresizingMask,
-            clipsSubviews:                             view.clipsSubviews,
-            constraints:                               view.constraints,
-            contentMode:                               view.contentMode,
+            id:                                        view.id,
             customClass:                               view.customClass,
             customModule:                              view.customModule,
             customModuleProvider:                      view.customModuleProvider,
             restorationIdentifier:                     view.restorationIdentifier,
+            userDefinedRuntimeAttributes:              view.userDefinedRuntimeAttributes,
             userLabel:                                 view.userLabel,
             colorLabel:                                view.colorLabel,
-            isMisplaced:                               view.isMisplaced,
-            isAmbiguous:                               view.isAmbiguous,
-            verifyAmbiguity:                           view.verifyAmbiguity,
-            opaque:                                    view.opaque,
-            rect:                                      view.rect,
-            subviews:                                  view.subviews,
-            translatesAutoresizingMaskIntoConstraints: view.translatesAutoresizingMaskIntoConstraints,
+            accessibility:                             view.accessibility,
+            contentMode:                               view.contentMode,
+            semanticContentAttribute:                  view.semanticContentAttribute,
+            tag:                                       view.tag,
             userInteractionEnabled:                    view.userInteractionEnabled,
-            userDefinedRuntimeAttributes:              view.userDefinedRuntimeAttributes,
-            connections:                               view.connections,
-            variations:                                view.variations,
+            multipleTouchEnabled:                      view.multipleTouchEnabled,
+            alpha:                                     view.alpha,
             backgroundColor:                           view.backgroundColor,
             tintColor:                                 view.tintColor,
+            opaque:                                    view.opaque,
             hidden:                                    view.hidden,
-            alpha:                                     view.alpha,
+            clearsContextBeforeDrawing:                view.clearsContextBeforeDrawing,
+            clipsSubviews:                             view.clipsSubviews,
+            autoresizesSubviews:                       view.autoresizesSubviews,
+            rect:                                      view.rect,
+            translatesAutoresizingMaskIntoConstraints: view.translatesAutoresizingMaskIntoConstraints,
+            autoresizingMask:                          view.autoresizingMask,
+            directionalLayoutMargins:                  view.directionalLayoutMargins,
+            layoutMargins:                             view.layoutMargins,
+            preservesSuperviewLayoutMargins:           view.preservesSuperviewLayoutMargins,
+            layoutMarginsFollowReadableWidth:          view.layoutMarginsFollowReadableWidth,
+            insetsLayoutMarginsFromSafeArea:           view.insetsLayoutMarginsFromSafeArea,
+            horizontalHuggingPriority:                 view.horizontalHuggingPriority,
+            verticalHuggingPriority:                   view.verticalHuggingPriority,
+            horizontalCompressionResistancePriority:   view.horizontalCompressionResistancePriority,
+            verticalCompressionResistancePriority:     view.verticalCompressionResistancePriority,
+            constraints:                               view.constraints,
+            connections:                               view.connections,
+            variations:                                view.variations,
+            subviews:                                  view.subviews,
+            verifyAmbiguity:                           view.verifyAmbiguity,
+            isMisplaced:                               view.isMisplaced,
+            isAmbiguous:                               view.isAmbiguous,
             axis:                                      container.attributeIfPresent(of: .axis) ?? "horizontal",
             distribution:                              container.attributeIfPresent(of: .distribution),
             alignment:                                 container.attributeIfPresent(of: .alignment),
             spacing:                                   container.attributeIfPresent(of: .spacing),
             baselineRelativeArrangement:               container.attributeIfPresent(of: .baselineRelativeArrangement),
-            horizontalCompressionResistancePriority:   view.horizontalCompressionResistancePriority,
-            verticalCompressionResistancePriority:     view.verticalCompressionResistancePriority,
-            horizontalHuggingPriority:                 view.horizontalHuggingPriority,
-            verticalHuggingPriority:                   view.verticalHuggingPriority,
-            accessibility:                             view.accessibility,
-            tag:                                       view.tag,
-            autoresizesSubviews:                       view.autoresizesSubviews,
-            clearsContextBeforeDrawing:                view.clearsContextBeforeDrawing,
-            multipleTouchEnabled:                      view.multipleTouchEnabled,
-            semanticContentAttribute:                  view.semanticContentAttribute,
-            preservesSuperviewLayoutMargins:           view.preservesSuperviewLayoutMargins,
-            layoutMarginsFollowReadableWidth:          view.layoutMarginsFollowReadableWidth,
-            insetsLayoutMarginsFromSafeArea:           view.insetsLayoutMarginsFromSafeArea,
-            directionalLayoutMargins:                  view.directionalLayoutMargins,
-            layoutMargins:                             view.layoutMargins,
         )
     }
 }
