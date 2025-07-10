7d9596143a7385e061fb8b8318b538607c9bf770
Blazej Sleboda <5544365+Adobels@users.noreply.github.com>
2025-07-10 16:44:53 +0200

    Add geting collection of sut from rootview of view controller, usefull for debug testing

--- Tests/DecoderTests/TestHelpers.swift
+++ Tests/DecoderTests/TestHelpers.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  TestHelpers.swift
 //  StoryboardDecoder
 //
 //  Created by Blazej Sleboda on 09/07/2025.
@@ -8,10 +8,17 @@
 import StoryboardDecoder
 import Foundation
 
-func sut<T: ViewProtocol>(from file: String) throws -> T {
-    let url = Bundle.module.url(forResource: file, withExtension: "xml")!
+func sut<T: IBElement>(from file: String) throws -> T {
+    guard let url = Bundle.module.url(forResource: file, withExtension: "xml") else { throw IBError.elementNotFound }
     let file = try StoryboardFile(url: url)
     guard let viewController = file.document.scenes?.first?.viewController?.viewController else { throw IBError.elementNotFound }
     guard let sut = viewController.rootView?.children(of: T.self).first else { throw IBError.elementNotFound }
+    return sut
+}
+func suts<T: IBElement>(from file: String) throws -> [T] {
+    guard let url = Bundle.module.url(forResource: file, withExtension: "xml") else { throw IBError.elementNotFound }
+    let file = try StoryboardFile(url: url)
+    guard let viewController = file.document.scenes?.first?.viewController?.viewController else { throw IBError.elementNotFound }
+    guard let sut = viewController.rootView?.children(of: T.self) else { throw IBError.elementNotFound }
     return sut
 }
